d622ba6c2d93330a1372cbacc64436f19473d939
Akili Inniss <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-04-11 10:19:10 -0400

    setting up server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const PokemonData = require('./models/Pokemon')
+const TrainerData = require('./models/Trainer')
+const TeamData = require('./models/Team');
+const { createEngine } = require('express-react-views');
+
+
+const app = express();
+const PORT = 5000 
+
+//Middleware
+app.set('view engine', 'jsx')
+app.engine('jsx',require('express-react-views').
+createEngine())
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(methodOverride('_method'))
+app.use(express.static('public'))
+
+//Routing 
+app.get('/', async (req, res) =>{
+
+})
