05332197948128e7f7b7267f88e12faf77b0acac
Akili Inniss <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-04-12 20:06:46 -0400

    set up connection to backend

---
+++ frontend/src/Components/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Index() {
+    return (
+    <div>
+        <div className=''>
+            <h1 className=''>Pokemon Guide</h1>
+        </div>
+    </div>
+    )
+
+}
