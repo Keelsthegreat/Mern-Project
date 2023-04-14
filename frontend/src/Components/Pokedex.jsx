8c2d21b78487bb251767454bf3f5d9242a00d9af
Akili Inniss <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-04-14 09:37:35 -0400

    .

--- frontend/src/Components/Pokedex.jsx
+++ frontend/src/Components/Pokedex.jsx
@@ -4,5 +4,32 @@ import axios from 'axios';
 
 
 function pkdex() {
+  
+        const [pdex, setPdex] = useState ([]);
+    
+        const [selectedPdex, setSelectedPdex] = useState(null)
+    
+    
+        useEffect(() => {
+            async function fetchData() {
+                const response = await axios.get('https://pokeapi.co/api/v2/pokemon/name')
+                //api link ^
+                setPdex(Object.values(response.data))
+                console.log(pdex)
+            }
+            fetchData()
+        }, []);
+    
+    function handleClick(pd) {
+        setSelectedPdex(pd)
+    }
+    
+    
+    return (
+        <div className='container'>
+            <h1 className='header'>Pokedex</h1>
+        </div>
+    )
+    }
 
-}
+
