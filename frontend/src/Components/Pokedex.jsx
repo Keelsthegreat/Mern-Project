9e999189cfd3aa8139b4c6fe6ab5b63c154bf699
Akili Inniss <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-04-15 13:02:56 -0400

    pdex

--- frontend/src/Components/Pokedex.jsx
+++ frontend/src/Components/Pokedex.jsx
@@ -1,35 +1,49 @@
-import React, { useState , useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+const pdex = () => {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
 
+  useEffect(() => {
+    const fetchPokemonList = async () => {
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
+      setPokemonList(response.data.results);
+    };
+    fetchPokemonList();
+  }, []);
 
-function pkdex() {
-  
-        const [pdex, setPdex] = useState ([]);
-    
-        const [selectedPdex, setSelectedPdex] = useState(null)
-    
-    
-        useEffect(() => {
-            async function fetchData() {
-                const response = await axios.get('https://pokeapi.co/api/v2/pokemon/name')
-                //api link ^
-                setPdex(Object.values(response.data))
-                console.log(pdex)
-            }
-            fetchData()
-        }, []);
-    
-    function handleClick(pd) {
-        setSelectedPdex(pd)
-    }
-    
-    
-    return (
-        <div className='container'>
-            <h1 className='header'>Pokedex</h1>
+  const handlePokemonClick = async (pokemon) => {
+    const response = await axios.get(pokemon.url);
+    setSelectedPokemon(response.data);
+  };
+
+  return (
+    <div>
+      <h1>Pokédex</h1>
+      <div className="pokemon-list">
+        {pokemonList.map((pokemon) => (
+          <div key={pokemon.name} onClick={() => handlePokemonClick(pokemon)}>
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`} alt={pokemon.name} />
+            <p>{pokemon.name}</p>
+          </div>
+        ))}
+      </div>
+      {selectedPokemon && (
+        <div className="pokemon-details">
+          <h2>{selectedPokemon.name}</h2>
+          <img src={selectedPokemon.sprites.front_default} alt={selectedPokemon.name} />
+          <p>Type: {selectedPokemon.types[0].type.name}</p>
+          <p>Height: {selectedPokemon.height / 10} m</p>
+          <p>Weight: {selectedPokemon.weight / 10} kg</p>
+          <p>Abilities:</p>
+          <ul>
+            {selectedPokemon.abilities.map((ability) => (
+              <li key={ability.ability.name}>{ability.ability.name}</li>
+            ))}
+          </ul>
         </div>
-    )
-    }
-
-
+      )}
+    </div>
+  );
+};
