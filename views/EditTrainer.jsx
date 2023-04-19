571f8abde108cfac6f982fdf123a8eb607cbb406
Akili Inniss <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-04-18 21:14:58 -0400

    .

--- views/EditTrainer.jsx
+++ views/EditTrainer.jsx
@@ -24,10 +24,7 @@ function EditTrainer(props) {
           <label htmlFor="image">Image:</label>
           <input type="url" id="image" name="image" defaultValue={trainer.image} />
         </div>
-        <form action = '/trainers/show'>
         <button type="submit">Save</button>
-
-        </form>
       </form>
    
     </div>
